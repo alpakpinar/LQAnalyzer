1393850f2d9a4cb106922416539caae4f6b0e869
Alp Akpinar <aakpinar@bu.edu>
2019-05-31 11:40:32 -0500

    Added parameter naming features

--- LQ_AODAnalyzer.py
+++ LQ_AODAnalyzer.py
@@ -9,14 +9,16 @@ ROOT.gSystem.Load("libFWCoreFWLite.so");
 ROOT.gSystem.Load("libDataFormatsFWLite.so");
 ROOT.FWLiteEnabler.enable()
 
-#Defining output file
-fout = ROOT.TFile("GENSIM_MET_1_4TeV_1_50METCut.root", "RECREATE")
+LQParams = '0_5TeV_0_7'
 
-LQMass = '1.4 TeV'
-LQCoupling = 1
+#Defining output file
+fout = ROOT.TFile('GENSIM_MET_' + LQParams +  '_50METCut.root', "RECREATE")
+
+LQMass = LQParams[0] + '.' + LQParams[2] + ' ' + LQParams[3:6]
+LQCoupling = LQParams[-3] + '.' + LQParams[-1]
 
 #Prepare the MET histogram
-MET_hist=ROOT.TH1F('MET', 'Missing Transverse Energy, M = ' + LQMass + ' #lambda = ' + str(LQCoupling), 27, 50, 1400)
+MET_hist=ROOT.TH1F('MET', 'Missing Transverse Energy, M = ' + LQMass + ' #lambda = ' + LQCoupling, 27, 50, 1400)
 MET_hist.GetXaxis().SetTitle('MET (GeV)')
 MET_hist.GetYaxis().SetTitle('Number of Events')
 
@@ -24,7 +26,7 @@ mets, metLabel = Handle("std::vector<reco::GenMET>"), "genMetTrue"
 
 #inputFile = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/SLQ_1_4TeV_1_noGenCut/SLQ_1_4TeV_1_LHEGS_noGenCut/190530_160018/0000/SLQ_1_4TeV_1_noGenCut_RunIIFall17wmLHEGS-00007_1.root'
 #inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/SLQ_1_4TeV_1_noGenCut/SLQ_1_4TeV_1_LHEGS_noGenCut/190530_160018/0000/' #No cuts
-inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/shape_study/SLQ_1_4TeV_1/SLQ_1_4TeV_1_LHEGS/190521_155428/0000' #50 GeV MET cut
+inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/shape_study/SLQ_0_5TeV_0_7/SLQ_0_5TeV_0_7_LHEGS/190526_233645/0000'
 
 file_count = 0
 
@@ -46,7 +48,7 @@ for inputFile in os.listdir(inputDir):
             MET_hist.Fill(mets.product()[0].pt())
 
 print('Normalizing MET histogram...')
-cross_section = 0.01542
+cross_section = 1.162
 luminosity = 35900
 num_events = 50000
 normWeight = cross_section*luminosity/num_events
@@ -56,7 +58,7 @@ MET_hist.Scale(normWeight)
 print('Drawing the histogram and saving the ROOT file...')
 canv = ROOT.TCanvas('canv', 'canv')
 MET_hist.Draw("hist")
-canv.Print('GENSIM_MET_1_4TeV_1_50METCut.png')
+canv.Print('savedPlots/METPlots/GENSIM_METPlots/GENSIM_MET_' + LQParams + '_50METCut.png')
 
 fout.Write()
 
