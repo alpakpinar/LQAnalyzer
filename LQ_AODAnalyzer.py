4581882ab26abb16fd5120487d36f436287f6592
Alp Akpinar <aakpinar@bu.edu>
2019-05-31 11:15:26 -0500

    Added new messages and datasets

--- LQ_AODAnalyzer.py
+++ LQ_AODAnalyzer.py
@@ -10,21 +10,25 @@ ROOT.gSystem.Load("libDataFormatsFWLite.so");
 ROOT.FWLiteEnabler.enable()
 
 #Defining output file
-fout = ROOT.TFile("GENSIM_MET_1_4TeV_1.root", "RECREATE")
+fout = ROOT.TFile("GENSIM_MET_1_4TeV_1_50METCut.root", "RECREATE")
 
 LQMass = '1.4 TeV'
 LQCoupling = 1
 
 #Prepare the MET histogram
-MET_hist=ROOT.TH1F('MET', 'Missing Transverse Energy, M = ' + LQMass + ' #lambda = ' + str(LQCoupling), 25, 0, 1400)
+MET_hist=ROOT.TH1F('MET', 'Missing Transverse Energy, M = ' + LQMass + ' #lambda = ' + str(LQCoupling), 27, 50, 1400)
 MET_hist.GetXaxis().SetTitle('MET (GeV)')
 MET_hist.GetYaxis().SetTitle('Number of Events')
 
 mets, metLabel = Handle("std::vector<reco::GenMET>"), "genMetTrue" 
 
 #inputFile = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/SLQ_1_4TeV_1_noGenCut/SLQ_1_4TeV_1_LHEGS_noGenCut/190530_160018/0000/SLQ_1_4TeV_1_noGenCut_RunIIFall17wmLHEGS-00007_1.root'
-inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/SLQ_1_4TeV_1_noGenCut/SLQ_1_4TeV_1_LHEGS_noGenCut/190530_160018/0000/'
+#inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/SLQ_1_4TeV_1_noGenCut/SLQ_1_4TeV_1_LHEGS_noGenCut/190530_160018/0000/' #No cuts
+inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/shape_study/SLQ_1_4TeV_1/SLQ_1_4TeV_1_LHEGS/190521_155428/0000' #50 GeV MET cut
+
 file_count = 0
+
+print('Starting analyzing files in %s' % inputDir)
 
 for inputFile in os.listdir(inputDir):
     if 'inLHE' not in inputFile:
@@ -44,7 +48,7 @@ for inputFile in os.listdir(inputDir):
 print('Normalizing MET histogram...')
 cross_section = 0.01542
 luminosity = 35900
-num_events = 49500
+num_events = 50000
 normWeight = cross_section*luminosity/num_events
 
 MET_hist.Scale(normWeight)
@@ -52,7 +56,8 @@ MET_hist.Scale(normWeight)
 print('Drawing the histogram and saving the ROOT file...')
 canv = ROOT.TCanvas('canv', 'canv')
 MET_hist.Draw("hist")
-canv.Print('GENSIM_MET_1_4TeV_1.png')
+canv.Print('GENSIM_MET_1_4TeV_1_50METCut.png')
 
 fout.Write()
+
 
