75bef604c39b39ed8fa890f15512d0f05d5c0a5a
Alp Akpinar <aakpinar@bu.edu>
2019-06-01 11:24:05 -0500

    Description added, paths to ROOT files modified

--- eventCountGraph.py
+++ eventCountGraph.py
@@ -1,13 +1,16 @@
+#Script for drawing the event yield graph for Higgs samples
+
 import ROOT
 from numpy import arange
 
-filename = 'LQ_1_4TeV_1_histOut_all.root'
+#filename = 'LQ_1_4TeV_1_histOut_all.root'
+filename = 'higgsRootFiles/Higgs2017_noHLTMET100_histOut_all.root' 
 infile = ROOT.TFile.Open(filename, 'UPDATE')
 mydir = infile.plots
 mydir.cd()
 
 evtCountsGraph = mydir.Get('evtCounts')
-cut_names = ['No Cut', 'HLT', 'Filters', 'MET', 'LJ_pT,eta', 'LJ_CHF,NHF', 'CaloMET-PFMET', 'Loose Ph', 'Loose El', 'Loose Mu', 'Loose Tau', 'Jet-MET Phi', 'b-jet'] 
+cut_names = ['No Cut', 'Filters', 'MET', 'LJ_pT,eta', 'LJ_CHF,NHF', 'CaloMET-PFMET', 'Loose Ph', 'Loose El', 'Loose Mu', 'Loose Tau', 'Jet-MET Phi', 'b-jet'] 
 x = arange(evtCountsGraph.GetN())
 
 evtCountsGraph.SetMarkerStyle(21)
@@ -18,7 +21,7 @@ for i in range(evtCountsGraph.GetN()):
     evtCountsGraph.SetPoint(i, x[i], evtCountsGraph.GetY()[i]/num_events)      
 
 x_ax = evtCountsGraph.GetXaxis()
-x_ax.Set(13,0,12)
+x_ax.Set(12,0,11)
 
 for i in range(len(cut_names)):
     x_ax.SetBinLabel(i+1, cut_names[i])
@@ -28,6 +31,6 @@ canv1 = ROOT.TCanvas("canv1","canv1")
 canv1.SetGrid()
 
 evtCountsGraph.Draw("AP")
-canv1.Print("evtCounts_1_4TeV_1.png")
+canv1.Print("evtCounts_Higgs_noHLTMET100.png")
 evtCountsGraph.Write()
 
