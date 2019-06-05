9aa86828b95f7157bad8dedaec5ff4d5e0785540
Alp Akpinar <aakpinar@bu.edu>
2019-06-05 11:08:35 -0500

    Added numLQ histogram for low MET studies

--- LQ_AODAnalyzer.py
+++ LQ_AODAnalyzer.py
@@ -9,24 +9,30 @@ ROOT.gSystem.Load("libFWCoreFWLite.so");
 ROOT.gSystem.Load("libDataFormatsFWLite.so");
 ROOT.FWLiteEnabler.enable()
 
-LQParams = '0_5TeV_0_7'
+LQParams = '1_4TeV_1'
 
 #Defining output file
 fout = ROOT.TFile('GENSIM_MET_' + LQParams +  '_50METCut.root', "RECREATE")
 
-LQMass = LQParams[0] + '.' + LQParams[2] + ' ' + LQParams[3:6]
-LQCoupling = LQParams[-3] + '.' + LQParams[-1]
+LQMass = '1.4 TeV'
+LQCoupling = '1.0'
 
 #Prepare the MET histogram
-MET_hist=ROOT.TH1F('MET', 'Missing Transverse Energy, M = ' + LQMass + ' #lambda = ' + LQCoupling, 27, 50, 1400)
+MET_hist=ROOT.TH1F('MET', 'Missing Transverse Energy, M = ' + LQMass + ' #lambda = ' + LQCoupling, 27, 0, 1400)
 MET_hist.GetXaxis().SetTitle('MET (GeV)')
 MET_hist.GetYaxis().SetTitle('Number of Events')
 
+#Prepare the numLQ histogram
+numLQ_hist=ROOT.TH1F('numLQ', 'Number of Leptoquarks, MET < 50 GeV', 4, 0, 4)
+numLQ_hist.GetXaxis().SetTitle('Number of Leptoquarks')
+numLQ_hist.GetYaxis().SetTitle('Number of Events')
+
 mets, metLabel = Handle("std::vector<reco::GenMET>"), "genMetTrue" 
+genParticles, genParticleLabel = Handle("std::vector<reco::GenParticle>"), "genParticles"
 
 #inputFile = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/SLQ_1_4TeV_1_noGenCut/SLQ_1_4TeV_1_LHEGS_noGenCut/190530_160018/0000/SLQ_1_4TeV_1_noGenCut_RunIIFall17wmLHEGS-00007_1.root'
-#inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/SLQ_1_4TeV_1_noGenCut/SLQ_1_4TeV_1_LHEGS_noGenCut/190530_160018/0000/' #No cuts
-inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/shape_study/SLQ_0_5TeV_0_7/SLQ_0_5TeV_0_7_LHEGS/190526_233645/0000'
+inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/SLQ_1_4TeV_1_noGenCut/SLQ_1_4TeV_1_LHEGS_noGenCut/190530_160018/0000/' #No cuts
+#inputDir = '/eos/uscms/store/user/aakpinar/SLQ_MCProduction/shape_study/SLQ_2TeV_0_5/SLQ_2TeV_0_5_LHEGS/190525_193828/0000'
 
 file_count = 0
 
@@ -45,20 +51,45 @@ for inputFile in os.listdir(inputDir):
         for event in events:
 
             event.getByLabel(metLabel, mets)
+	    event.getByLabel(genParticleLabel, genParticles)
+
             MET_hist.Fill(mets.product()[0].pt())
 
+	    numLQ = 0
+
+	    if mets.product()[0].pt() < 50:
+
+	        for particle in genParticles.product():
+		 	    
+		    if abs(particle.pdgId()) == 1104 and particle.status() == 22:
+
+	    	        numLQ += 1
+
+	    numLQ_hist.Fill(numLQ)
+
 print('Normalizing MET histogram...')
-cross_section = 1.162
+
+cross_section = 0.06559 #for the no-cut sample
+#cross_section = 0.0004373
 luminosity = 35900
 num_events = 50000
 normWeight = cross_section*luminosity/num_events
 
 MET_hist.Scale(normWeight)
 
-print('Drawing the histogram and saving the ROOT file...')
+print('Normalizing numLQ histogram...')
+
+numLQ_hist.Scale(normWeight)
+numLQ_hist.SetNdivisions(505)
+
+print('Drawing the histograms and saving the ROOT file...')
 canv = ROOT.TCanvas('canv', 'canv')
 MET_hist.Draw("hist")
-canv.Print('savedPlots/METPlots/GENSIM_METPlots/GENSIM_MET_' + LQParams + '_50METCut.png')
+canv.Print('savedPlots/METPlots/GENSIM_METPlots/GENSIM_MET_' + LQParams + '_noMETCut.png')
+
+canv2 = ROOT.TCanvas('canv2', 'canv2')
+numLQ_hist.Draw("hist")
+canv2.Print("savedPlots/METPlots/GENSIM_METPlots/GENSIM_numLQ_" + LQParams + '_noMETCut.png') 
 
 fout.Write()
 
