428ae6ee000a26e3698a8fb9c5344af8f91d5373
Alp Akpinar <aakpinar@bu.edu>
2019-06-06 14:49:26 -0500

    Added new axis labeling features, increased bottom margin

--- eventCountGraph.py
+++ eventCountGraph.py
@@ -1,36 +1,45 @@
-#Script for drawing the event yield graph for Higgs samples
+#Script for drawing the event yield graph for LQ samples
 
 import ROOT
 from numpy import arange
 
-#filename = 'LQ_1_4TeV_1_histOut_all.root'
-filename = 'higgsRootFiles/Higgs2017_noHLTMET100_histOut_all.root' 
+filename = 'LQRootFiles/LQ_1_4TeV_1_histOut_all.root'
 infile = ROOT.TFile.Open(filename, 'UPDATE')
 mydir = infile.plots
 mydir.cd()
 
-evtCountsGraph = mydir.Get('evtCounts')
-cut_names = ['No Cut', 'Filters', 'MET', 'LJ_pT,eta', 'LJ_CHF,NHF', 'CaloMET-PFMET', 'Loose Ph', 'Loose El', 'Loose Mu', 'Loose Tau', 'Jet-MET Phi', 'b-jet'] 
+evtCountsGraph = mydir.Get('evtCounts;1')
+cut_names = ['No Cut', 'HLT', 'Filters', 'Loose Mu', 'Loose El', 'Loose Ph', 'Loose Tau', 'MET', 'LJ_pT,eta', 'LJ_CHF,NHF', 'CaloMET-PFMET', 'Jet-MET Phi', 'b-jet'] 
 x = arange(evtCountsGraph.GetN())
 
 evtCountsGraph.SetMarkerStyle(21)
+
 num_events = evtCountsGraph.GetY()[0]
 #print(evtCountsGraph.GetN())
 
 for i in range(evtCountsGraph.GetN()):
-    evtCountsGraph.SetPoint(i, x[i], evtCountsGraph.GetY()[i]/num_events)      
+    evtCountsGraph.SetPoint(i, x[i], evtCountsGraph.GetY()[i]*100/num_events)      
 
 x_ax = evtCountsGraph.GetXaxis()
-x_ax.Set(12,0,11)
+x_ax.Set(13,0,13)
 
 for i in range(len(cut_names)):
     x_ax.SetBinLabel(i+1, cut_names[i])
 
-#Event Counts
-canv1 = ROOT.TCanvas("canv1","canv1")
+#x_ax.CenterLabels()
+x_ax.LabelsOption("v")
+x_ax.SetTitle('Cuts')
+x_ax.SetTitleOffset(1.4)
+x_ax.SetLabelSize(0.03)
+
+
+evtCountsGraph.GetYaxis().SetTitle('% Events Passing')
+
+canv1 = ROOT.TCanvas("canv1","canv1", 800, 600)
 canv1.SetGrid()
+canv1.SetBottomMargin(0.20)
 
 evtCountsGraph.Draw("AP")
-canv1.Print("evtCounts_Higgs_noHLTMET100.png")
+canv1.Print("evtCounts_LQ_1_4TeV_1.png")
 evtCountsGraph.Write()
 
