431eb27256ab5c8c29892b55668fe0a266cbae19
Alp Akpinar <aakpinar@bu.edu>
2019-06-16 14:53:09 -0500

    First commit

---
+++ sleptoquark_uv_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.27
+# Mathematica version: 9.0 for Linux x86 (64-bit) (November 20, 2012)
+# Date: Mon 25 Sep 2017 14:48:48
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ sleptoquark_uv_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.27
+# Mathematica version: 9.0 for Linux x86 (64-bit) (November 20, 2012)
+# Date: Mon 25 Sep 2017 14:48:48
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
